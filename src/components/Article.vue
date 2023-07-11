6aa0f099c316f37cf473474de23528b8d280e01c
Manoj Raju <mraju10@asu.edu>
2023-07-11 15:46:40 +0530

    basic

---
+++ src/components/Article.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Article</h2>
+        <h3>{{ title }}{{ likes }} Published {{ pub ? yes : no  }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Article-comp",
+        props:{
+            title:{
+                type:String,
+                required:true,
+                default:"default"
+            },
+            likes:Number,
+            pub:Boolean,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Greet.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>HI HOW ARE YOU?</h1>
+    <h2>{{ msg }} - {{ msg1 }} -{{ wish }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Greet-component" ,
+  props: ["msg","msg1","wish"],
+};
+</script>
